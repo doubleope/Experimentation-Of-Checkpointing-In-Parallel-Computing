7db5c2bf21ef7e2b2ae5a4880b0607539f89895b
Opeyemi Openiyi <44436984+doubleope@users.noreply.github.com>
2020-03-31 01:44:58 -0500

    Update exact_entries_two_nodes.sh

--- exact_entries_two_nodes.sh
+++ exact_entries_two_nodes.sh
@@ -125,3 +125,31 @@ systemctl restart gmond
 wwsh pxe update
 
 yum -y install ohpc-autotools
+yum -y install EasyBuild-ohpc
+yum -y install hwloc-ohpc
+yum -y install spack-ohpc
+yum -y install valgrind-ohpc
+
+
+yum -y install gnu8-compilers-ohpc
+yum -y install llvm5-compilers-ohpc
+yum -y install openmpi3-gnu8-ohpc mpich-gnu8-ohpc
+yum -y install mvapich2-gnu8-ohpc
+yum -y install ohpc-gnu8-perf-tools
+yum -y install ohpc-gnu8-geopm
+yum -y install lmod-defaults-gnu8-openmpi3-ohpc
+yum search petsc-gnu7 ohpc
+
+yum -y install ohpc-gnu8-serial-libs
+yum -y install ohpc-gnu8-io-libs
+yum -y install ohpc-gnu8-python-libs
+yum -y install ohpc-gnu8-runtimes
+yum -y install ohpc-gnu8-mpich-parallel-libs
+yum -y install ohpc-gnu8-openmpi3-parallel-libs
+
+systemctl enable munge
+systemctl enable slurmctld
+systemctl start munge
+systemctl start slurmctld
+
+
