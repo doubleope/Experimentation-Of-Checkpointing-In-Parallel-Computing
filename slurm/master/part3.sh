1d05a266d9af3fa1434474284ebd336ef92c1ec3
Opeyemi Openiyi <44436984+doubleope@users.noreply.github.com>
2020-04-01 18:13:01 -0500

    Update part3.sh

--- slurm/master/part3.sh
+++ slurm/master/part3.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
-yum install epel-release
-yum install munge munge-libs munge-devel -y
-yum install rng-tools -y
-rngd -r /dev/urandom
-/usr/sbin/create-munge-key -r
-dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key
-chown munge: /etc/munge/munge.key
-chmod 400 /etc/munge/munge.key
+yum install openssl openssl-devel pam-devel numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel man2html libibmad libibumad -y
+mkdir /nfs
+cd /nfs
+wget https://download.schedmd.com/slurm/slurm-20.02.1.tar.bz2
+yum install perl-devel perl-CPAN
+rpmbuild -ta slurm-20.02.1.tar.bz2
+cd /root/rpmbuild/RPMS/x86_64  
+mkdir /nfs/slurm-rpms   
+cp -a . /nfs/slurm-rpms  
+
+yum --nogpgcheck localinstall slurm-20.02.1-1.el7.x86_64.rpm slurm-libpmi-20.02.1-1.el7.x86_64.rpm slurm-slurmctld-20.02.1-1.el7.x86_64.rpm slurm-contribs-20.02.1-1.el7.x86_64.rpm slurm-openlava-20.02.1-1.el7.x86_64.rpm slurm-slurmd-20.02.1-1.el7.x86_64.rpm slurm-devel-20.02.1-1.el7.x86_64.rpm slurm-pam_slurm-20.02.1-1.el7.x86_64.rpm slurm-slurmdbd-20.02.1-1.el7.x86_64.rpm slurm-example-configs-20.02.1-1.el7.x86_64.rpm slurm-perlapi-20.02.1-1.el7.x86_64.rpm slurm-torque-20.02.1-1.el7.x86_64.rpm
+
+cd /etc/slurm
+wget https://raw.githubusercontent.com/doubleope/Up-To-Par-Alell/master/slurm/slurm.conf 
+mkdir /var/spool/slurmctld
+chown slurm: /var/spool/slurmctld
+chmod 755 /var/spool/slurmctld
+touch /var/log/slurmctld.log
+chown slurm: /var/log/slurmctld.log
+touch /var/log/slurm_jobacct.log /var/log/slurm_jobcomp.log
+chown slurm: /var/log/slurm_jobacct.log /var/log/slurm_jobcomp.log
