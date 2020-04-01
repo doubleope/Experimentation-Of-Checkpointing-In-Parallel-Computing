905d9710df64ced2fac430413b068a2c14677bff
Opeyemi Openiyi <44436984+doubleope@users.noreply.github.com>
2020-04-01 17:52:50 -0500

    Update part2.sh

--- slurm/master/part2.sh
+++ slurm/master/part2.sh
@@ -6,3 +6,13 @@ useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g m
 export SLURMUSER=992
 groupadd -g $SLURMUSER slurm
 useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
+
+yum install epel-release
+yum install munge munge-libs munge-devel -y
+yum install rng-tools -y
+rngd -r /dev/urandom
+/usr/sbin/create-munge-key -r
+
+dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key
+chown munge: /etc/munge/munge.key
+chmod 400 /etc/munge/munge.key
