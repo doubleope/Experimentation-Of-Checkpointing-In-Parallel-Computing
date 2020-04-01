0b9fa5752276066ed460355aba8204096182a245
Opeyemi Openiyi <44436984+doubleope@users.noreply.github.com>
2020-04-01 17:03:04 -0500

    Update compute_nodes.sh

--- slurm/compute_nodes.sh
+++ slurm/compute_nodes.sh
@@ -27,5 +27,25 @@ cd /root/rpmbuild/RPMS/x86_64
 mkdir /nfs/slurm-rpms   
 cp -a . /nfs/slurm-rpms  
 
+yum --nogpgcheck localinstall slurm-20.02.1-1.el7.x86_64.rpm slurm-libpmi-20.02.1-1.el7.x86_64.rpm slurm-slurmctld-20.02.1-1.el7.x86_64.rpm slurm-contribs-20.02.1-1.el7.x86_64.rpm slurm-openlava-20.02.1-1.el7.x86_64.rpm slurm-slurmd-20.02.1-1.el7.x86_64.rpm slurm-devel-20.02.1-1.el7.x86_64.rpm slurm-pam_slurm-20.02.1-1.el7.x86_64.rpm slurm-slurmdbd-20.02.1-1.el7.x86_64.rpm slurm-example-configs-20.02.1-1.el7.x86_64.rpm slurm-perlapi-20.02.1-1.el7.x86_64.rpm slurm-torque-20.02.1-1.el7.x86_64.rpm
+cd /etc/slurm
+wget https://raw.githubusercontent.com/doubleope/Up-To-Par-Alell/master/slurm/slurm.conf 
+mkdir /var/spool/slurmd
+chown slurm: /var/spool/slurmd
+chmod 755 /var/spool/slurmd
+touch /var/log/slurmd.log
+chown slurm: /var/log/slurmd.log
 
+slurmd -C
 
+systemctl stop firewalld
+systemctl disable firewalld
+
+yum install ntp -y
+chkconfig ntpd on
+ntpdate pool.ntp.org
+systemctl start ntpd
+
+systemctl enable slurmd.service
+systemctl start slurmd.service
+systemctl status slurmd.service
