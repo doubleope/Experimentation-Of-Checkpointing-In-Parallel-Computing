beb863e5b6b1a72738d0e533da3748f7d53ac4d4
Opeyemi Openiyi <44436984+doubleope@users.noreply.github.com>
2020-04-01 17:50:02 -0500

    Create part3.sh

---
+++ slurm/master/part3.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+yum install epel-release
+yum install munge munge-libs munge-devel -y
+yum install rng-tools -y
+rngd -r /dev/urandom
+/usr/sbin/create-munge-key -r
+dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key
+chown munge: /etc/munge/munge.key
+chmod 400 /etc/munge/munge.key
