558d01b2fa641fe7389026a1f149c3f86492b994
Opeyemi Openiyi <44436984+doubleope@users.noreply.github.com>
2020-04-08 20:48:59 -0500

    Update ping_pong.c

--- mpi/ping_pong.c
+++ mpi/ping_pong.c
@@ -10,14 +10,50 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "scr.h"
+
+int TIMESTEPS =100;
+
+void checkpoint(state) {
+  /* Tell SCR that you are getting ready to start a checkpoint phase */
+  SCR_Start_checkpoint();
+
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  char file[256];
+  /* create your checkpoint file name */
+  sprintf(file, "rank_%d.ckpt", rank);
+
+  /* Call SCR_Route_file to request a new file name (scr_file) that will cause
+     your application to write the file to a fast tier of storage, e.g.,
+     a burst buffer */
+  char scr_file[SCR_MAX_FILENAME];
+  SCR_Route_file(file, scr_file);
+
+  /* Use the new file name to perform your checkpoint I/O */
+  FILE* fs = fopen(scr_file, "w");
+  if (fs != NULL) {
+    fwrite(state, 1, sizeof(state), fs);
+    fclose(fs);
+  }
+
+  /* Tell SCR that you are done with your checkpoint phase */
+  SCR_Complete_checkpoint(1);
+  return;
+}
 
 int main(int argc, char** argv) {
   const int PING_PONG_LIMIT = 10;
 
   // Initialize the MPI environment
   MPI_Init(NULL, NULL);
-  // Find out rank, size
-  int world_rank;
+  SCR_Init();
+  int state;
+   for(int t = 0; t < TIMESTEPS; t++)
+  {
+    /* ... Do work ... */
+     int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -44,5 +80,14 @@ int main(int argc, char** argv) {
              world_rank, ping_pong_count, partner_rank);
     }
   }
+    state = t;
+    int flag;
+    /* Ask SCR if we should take a checkpoint now */
+    SCR_Need_checkpoint(&flag);
+    if (flag)
+      checkpoint(state);
+  }
+  // Find out rank, size
+  SCR_Finalize();
   MPI_Finalize();
 }
