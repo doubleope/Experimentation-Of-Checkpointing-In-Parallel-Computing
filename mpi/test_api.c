4ff5f8e2c9160837a2a5d8372075156e4ce9ca64
Opeyemi Openiyi <44436984+doubleope@users.noreply.github.com>
2020-04-16 15:58:46 -0500

    Update test_api.c

--- mpi/test_api.c
+++ mpi/test_api.c
@@ -32,8 +32,8 @@ int output = 0;
 char *path = NULL;
 int use_scr = 1;
 
-int rank = -1;
-int ranks = 0;
+int world_rank;
+int world_size;
 
 int timestep = 0;
 
@@ -424,9 +424,7 @@ void print_usage()
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
-    int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     
 
