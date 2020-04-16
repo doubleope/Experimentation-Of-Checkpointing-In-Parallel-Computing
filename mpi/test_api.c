a390fd5345a4b61f6370fd3b7a16520803d91b6d
Opeyemi Openiyi <44436984+doubleope@users.noreply.github.com>
2020-04-16 16:04:24 -0500

    Update test_api.c

--- mpi/test_api.c
+++ mpi/test_api.c
@@ -32,8 +32,8 @@ int output = 0;
 char *path = NULL;
 int use_scr = 1;
 
-int world_rank;
-int world_size;
+int rank;
+int ranks;
 
 int timestep = 0;
 
@@ -209,23 +209,23 @@ double getbw(char *name, char *buf, size_t size, int times)
             }
 
             int ping_pong_count = 0;
-            int partner_rank = (world_rank + 1) % 2;
+            int partner_rank = (rank + 1) % 2;
             while (ping_pong_count < PING_PONG_LIMIT)
             {
-                if (world_rank == ping_pong_count % 2)
+                if (rank == ping_pong_count % 2)
                 {
                     // Increment the ping pong count before you send it
                     ping_pong_count++;
                     MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
                     printf("%d sent and incremented ping_pong_count %d to %d\n",
-                        world_rank, ping_pong_count, partner_rank);
+                        rank, ping_pong_count, partner_rank);
                 }
                 else
                 {
                     MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
                             MPI_STATUS_IGNORE);
                     printf("%d received ping_pong_count %d from %d\n",
-                        world_rank, ping_pong_count, partner_rank);
+                        rank, ping_pong_count, partner_rank);
                 }
             }
 
