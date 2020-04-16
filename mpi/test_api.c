b3ea64b187595e2a07140900c0f92aebba898dd8
Opeyemi Openiyi <44436984+doubleope@users.noreply.github.com>
2020-04-16 13:06:44 -0500

    Add ping pong

--- mpi/test_api.c
+++ mpi/test_api.c
@@ -195,6 +195,30 @@ double getbw(char* name, char* buf, size_t size, int times)
       int rc;
       int valid = 0;
 
+      
+      if (world_size != 2) {
+        fprintf(stderr, "World size must be two for %s\n", argv[0]);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+      }
+
+  int ping_pong_count = 0;
+  int partner_rank = (world_rank + 1) % 2;
+  while (ping_pong_count < PING_PONG_LIMIT) {
+    if (world_rank == ping_pong_count % 2) {
+      // Increment the ping pong count before you send it
+      ping_pong_count++;
+      MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
+      printf("%d sent and incremented ping_pong_count %d to %d\n",
+             world_rank, ping_pong_count, partner_rank);
+    } else {
+      MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      printf("%d received ping_pong_count %d from %d\n",
+             world_rank, ping_pong_count, partner_rank);
+    }
+  }
+      
+      
 /*
       int need_checkpoint;
       SCR_Need_checkpoint(&need_checkpoint);
