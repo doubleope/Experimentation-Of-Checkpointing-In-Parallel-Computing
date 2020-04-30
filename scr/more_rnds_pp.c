eca4b974e6ecaf22d9468b37d037f16fe146c0bf
Opeyemi Openiyi <44436984+doubleope@users.noreply.github.com>
2020-04-30 13:11:16 -0500

    Update more_rnds_pp.c

--- scr/more_rnds_pp.c
+++ scr/more_rnds_pp.c
@@ -1,3 +1,4 @@
+
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +6,7 @@
 int main(int argc, char **argv)
 {
     const int PING_PONG_LIMIT = 10;
+    const int NUMBER_OF_ROUNDS = 3;
 
     // Initialize the MPI environment
     MPI_Init(NULL, NULL);
@@ -20,25 +22,27 @@ int main(int argc, char **argv)
         fprintf(stderr, "World size must be two for %s\n", argv[0]);
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
-
-    int ping_pong_count = 0;
-    int partner_rank = (world_rank + 1) % 2;
-    while (ping_pong_count < PING_PONG_LIMIT)
+    for (i = 1; i < NUMBER_OF_ROUNDS + 1; ++i)
     {
-        if (world_rank == ping_pong_count % 2)
+        int ping_pong_count = 0;
+        int partner_rank = (world_rank + 1) % 2;
+        while (ping_pong_count < PING_PONG_LIMIT)
         {
-            // Increment the ping pong count before you send it
-            ping_pong_count++;
-            MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
-            printf("%d sent and incremented ping_pong_count %d to %d\n",
-                   world_rank, ping_pong_count, partner_rank);
-        }
-        else
-        {
-            MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
-                     MPI_STATUS_IGNORE);
-            printf("%d received ping_pong_count %d from %d\n",
-                   world_rank, ping_pong_count, partner_rank);
+            if (world_rank == ping_pong_count % 2)
+            {
+                // Increment the ping pong count before you send it
+                ping_pong_count++;
+                MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
+                printf("%d sent and incremented ping_pong_count %d to %d\n",
+                       world_rank, ping_pong_count, partner_rank);
+            }
+            else
+            {
+                MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
+                         MPI_STATUS_IGNORE);
+                printf("%d received ping_pong_count %d from %d\n",
+                       world_rank, ping_pong_count, partner_rank);
+            }
         }
     }
     MPI_Finalize();
